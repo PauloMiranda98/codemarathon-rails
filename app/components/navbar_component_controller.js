cc493222135397a81d68e99fbb70d83187fe6053
Paulo Miranda <paulomirandamss12@gmail.com>
2022-09-10 15:04:14 -0300

    add assets precompile when run ./bin/dev

--- app/components/navbar_component_controller.js
+++ app/components/navbar_component_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!!!", this.element)
+    console.log("Hello, Stimulus!", this.element)
   }
 }
