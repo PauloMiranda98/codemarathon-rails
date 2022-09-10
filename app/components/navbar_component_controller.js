a078766efa1aed801109f644c223d967c6d0c53a
Paulo Miranda <paulomirandamss12@gmail.com>
2022-09-10 17:33:09 -0300

    fix navbar

--- app/components/navbar_component_controller.js
+++ app/components/navbar_component_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["mobileMenuIcon"]
+
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+  }
+
+  toggleMobileIcon() {
+    this.mobileMenuIconTargets.forEach(element => {
+      element.classList.toggle("hidden");
+      element.classList.toggle("block");        
+    });
   }
 }
