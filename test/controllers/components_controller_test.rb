9d13567283e47c2fe40778e4a07e9d6102b62d54
Paulo Miranda <paulomirandamss12@gmail.com>
2022-11-13 13:41:32 -0300

    remove unused files

--- test/controllers/components_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ComponentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get components_index_url
-    assert_response :success
-  end
-end
