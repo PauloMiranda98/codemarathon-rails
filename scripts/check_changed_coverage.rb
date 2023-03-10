fea8fb55ebded2d06c3ab793917a67f0702b91bb
Paulo Miranda <paulomirandamss12@gmail.com>
2023-03-10 12:39:11 -0300

    testando..

---
+++ scripts/check_changed_coverage.rb
@@ -0,0 +1,8 @@
+changed_files = `git diff --name-only origin/main`.split("\n")
+
+File.open("code-coverage-results.md", "w") do |f| 
+  f.write "# Primeiro Comentário\n"
+  changed_files.each do |file_name|
+    f.write "- #{file_name}\n"
+  end
+end
